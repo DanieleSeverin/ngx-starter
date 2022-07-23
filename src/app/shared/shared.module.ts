c5ebe5370d62aca8eaef1ef539c87f3f82c70689
DanieleSeverin89 <daniele.severin@hbtsa.it>
2022-07-23 11:56:03 +0200

    Add empty Shell and User modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ShellComponent } from './shell/shell.component';
+
+const components = [
+  ShellComponent,
+];
+
+const modules = [
+  CommonModule,
+  RouterModule,
+];
+
+@NgModule({
+  declarations: [
+    ...components,
+  ],
+  imports: [
+    ...modules,
+  ],
+  exports: [
+    ...components,
+    ...modules,
+  ]
+})
+export class SharedModule { }
